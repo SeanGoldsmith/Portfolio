baf4c5f75c476df3dd248395cada614deedd9e71
SeanGoldsmith <Goldsmith.Sean90@gmail.com>
2017-11-20 13:13:15 -0800

    Fixed the multiple digits problem.  Calculator works as expected unless a string of operations is performed.  For example 3+3-2 will yield 8.  The operation sticks to whatever the first operator was.  I will investigate this.

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -3,82 +3,118 @@ nextNumberOption = 0; //case 0 means first number. case 1 is addition.
 // case two is subtraction
 // case three is multiplication
 // case 4 is division
+isFirstNumber = true;
+holdValue = 0;
 
 function updateNumber(someNumber) {
-	if (nextNumberOption == 0)
-	{
 		var view = document.getElementById("view");
 		currentState = view.innerHTML;
 		view.innerHTML = currentState + someNumber.toString();
-		currentTotal = someNumber;
+}
+
+function add () {
+	var view = document.getElementById("view");
+	viewNumber = parseInt(view.innerHTML)
+	if (isFirstNumber) 
+	{
+		holdValue = viewNumber;
+		nextNumberOption = 1;
+		isFirstNumber = false;
 	}
-	else if (nextNumberOption == 1)
+	else if (!isFirstNumber)
+	{
+		holdValue = calcNewHoldNumber();
+	}
+	view.innerHTML = "";
+}
+
+function subtract () {
+	var view = document.getElementById("view");
+	viewNumber = parseInt(view.innerHTML)
+	if (isFirstNumber) 
+	{
+		holdValue = viewNumber;
+		nextNumberOption = 2;
+		isFirstNumber = false;
+	}
+	else if (!isFirstNumber)
+	{
+		holdValue = calcNewHoldNumber();
+	}
+	view.innerHTML = "";
+}
+
+function multi () {
+	var view = document.getElementById("view");
+	viewNumber = parseInt(view.innerHTML)
+	if (isFirstNumber) 
+	{
+		holdValue = viewNumber;
+		nextNumberOption = 3;
+		isFirstNumber = false;
+	}
+	else if (!isFirstNumber)
+	{
+		holdValue = calcNewHoldNumber();
+	}
+	view.innerHTML = "";
+
+}
+
+function divide () {
+	var view = document.getElementById("view");
+	viewNumber = parseInt(view.innerHTML)
+	if (isFirstNumber) 
+	{
+		holdValue = viewNumber;
+		nextNumberOption = 4;
+		isFirstNumber = false;
+	}
+	else if (!isFirstNumber)
+	{
+		holdValue = calcNewHoldNumber();
+	}
+	view.innerHTML = "";
+}
+
+function runEquals() {
+	holdValue = calcNewHoldNumber();
+	view.innerHTML = holdValue;
+	isFirstNumber = true;
+}
+
+function clearAll() {
+	holdValue = 0;
+	nextNumberOption = 0;
+	isFirstNumber = true;
+	var view = document.getElementById("view");
+	view.innerHTML = "";
+}
+
+function calcNewHoldNumber() {
+	console.log(nextNumberOption);
+	if (nextNumberOption == 1)
 	{
 		var view = document.getElementById("view");
-		currentState = view.innerHTML;
-		view.innerHTML = currentState + someNumber.toString();
-		currentTotal += someNumber;
+		holdValue = holdValue + parseInt(view.innerHTML);
+		return holdValue;
 	}
 	else if (nextNumberOption == 2)
 	{
 		var view = document.getElementById("view");
-		currentState = view.innerHTML;
-		view.innerHTML = currentState + someNumber.toString();
-		currentTotal -= someNumber;
+		holdValue = holdValue - parseInt(view.innerHTML);
+		return holdValue;
 	}
 	else if (nextNumberOption == 3)
 	{
 		var view = document.getElementById("view");
-		currentState = view.innerHTML;
-		view.innerHTML = currentState + someNumber.toString();
-		currentTotal *= someNumber;
+		holdValue = holdValue * parseInt(view.innerHTML);
+		return holdValue;
 	}
 	else if (nextNumberOption == 4)
 	{
 		var view = document.getElementById("view");
-		currentState = view.innerHTML;
-		view.innerHTML = currentState + someNumber.toString();
-		currentTotal /= someNumber;
+		holdValue = holdValue / parseInt(view.innerHTML);
+		return holdValue;
 	}
-
-
-}
-
-function add () {
-	nextNumberOption = 1;
-	var view = document.getElementById("view");
-	currentState = view.innerHTML;
-	view.innerHTML = currentState + "+";
-}
-
-function subtract () {
-	nextNumberOption = 2;
-	var view = document.getElementById("view");
-	currentState = view.innerHTML;
-	view.innerHTML = currentState + "-";
-}
-
-function multi () {
-	nextNumberOption = 3;
-	var view = document.getElementById("view");
-	currentState = view.innerHTML;
-	view.innerHTML = currentState + "*";
-}
-
-function divide () {
-	nextNumberOption = 4;
-	var view = document.getElementById("view");
-	currentState = view.innerHTML;
-	view.innerHTML = currentState + "/";
-}
-
-function printTotal() {
-	view.innerHTML = currentTotal;
-}
-
-function clearAll() {
-	currentTotal = 0;
-	nextNumberOption = 0;
-	var view = document.getElementById("view");
-	view.innerHTML = "";
 }
